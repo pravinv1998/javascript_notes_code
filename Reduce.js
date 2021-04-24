83cecf6cba2a16bd49a061b9ecdc2043f2cb2169
pravinv1998 <pravin.wargad1998@gmail.com>
2021-04-24 12:47:38 +0530

    new updates

--- Reduce.js
+++ Reduce.js
@@ -1,4 +1,4 @@
-let arr = [1, 2, 3, 4, 5]
-// addition operation and initial state value 
-const num = arr.reduce((acumulator, elem) => (acumulator += elem), 3)
-console.log(num)
+let arr = [1, 2, 3, 4, 5];
+// addition operation and initial state value
+const num = arr.reduce((acumulator, elem) => (acumulator += elem), 3);
+console.log(num);
